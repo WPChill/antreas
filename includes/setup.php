6c2507226633d5379c96017c8b216c2eef94160d
unknown <cristi.ecmecian@yahoo.com>
2018-10-21 14:26:00 +0300

    removed custom css

--- includes/setup.php
+++ includes/setup.php
@@ -13,19 +13,3 @@ function cpotheme_background_args( $data ) {
 
 	return $data;
 }
-
-
-add_action( 'wp_head', 'cpotheme_styling_custom', 20 );
-function cpotheme_styling_custom() {
-	?>
-	<style type="text/css">
-	
-		html body .button:hover,
-		html body input[type=submit]:hover { 
-			color: #fff; background: #121212; 
-		}  
-	
-	</style>
-	<?php
-}
-
