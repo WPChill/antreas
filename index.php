e998145c2689cc849da57fa1d43bdfc800380b4c
unknown <cristi.ecmecian@yahoo.com>
2018-10-24 13:18:11 +0300

    fixed blog columns

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 
 <?php if ( antreas_show_posts() ) : ?>
 	<div id="main" class="main section">
-		<div class="container">	
-			<?php antreas_section_heading( 'blog' ); ?>	
+		<div class="container">
+			<?php antreas_section_heading( 'blog' ); ?>
 			<section id="content" class="content">
 				<?php do_action( 'antreas_before_content' ); ?>
-				<?php antreas_grid( null, 'element', 'blog', 2, array( 'class' => 'column-narrow' ) ); ?>
+				<?php antreas_grid( null, 'element', 'blog', 3, array( 'class' => 'column-narrow' ) ); ?>
 				<?php antreas_numbered_pagination(); ?>
 				<?php do_action( 'antreas_after_content' ); ?>
 			</section>
