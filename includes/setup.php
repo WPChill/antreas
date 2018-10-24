db95695c345b0ee1ae5d82750c32ef920260d894
cristianraiber <cristian@machothemes.com>
2018-10-24 12:18:41 +0300

    sets about pages when importing demo

--- includes/setup.php
+++ includes/setup.php
@@ -15,5 +15,23 @@ function antreas_background_args( $data ) {
 
 add_action( 'cpo_companion_import_done', 'antreas_import_done' );
 function antreas_import_done() {
+
+	// Sets background image from demo content
 	set_theme_mod( 'background_image', antreas_get_attachment_url_by_slug( 'background' ) );
+
+	// Sets about pages
+	$about_pages = array();
+	$page = get_page_by_path( 'our story' );
+	if ( $page ) {
+		$about_pages[] = $page->ID;
+	}
+	$page = get_page_by_path( 'our mission' );
+	if ( $page ) {
+		$about_pages[] = $page->ID;
+	}
+	$page = get_page_by_path( 'our beliefs' );
+	if ( $page ) {
+		$about_pages[] = $page->ID;
+	}
+	antreas_update_option( 'about_pages', $about_pages );
 }
