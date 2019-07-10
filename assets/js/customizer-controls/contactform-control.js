955a8d3954aa25d7f7345f9c7ff34e7e9bd782fe
Cosmin <10389279+cristeacosmin@users.noreply.github.com>
2019-07-10 23:39:12 +0300

    update js part as well

--- assets/js/customizer-controls/contactform-control.js
+++ assets/js/customizer-controls/contactform-control.js
@@ -6,29 +6,54 @@
 			var control = this,
 				$pluginSelect =  control.container.children('select'),
 				$wpformsContainer = control.container.find('.cpotheme_contact_control__wpforms'),
-				$cf7Container = control.container.find('.cpotheme_contact_control__cf7forms');
-
+				$cf7Container = control.container.find('.cpotheme_contact_control__cf7forms'),
+				$kaliformsContainer = control.container.find('.cpotheme_contact_control__kali-forms');
+			
 			if ( $pluginSelect.length && control.settings.plugin_select.get() === 'wpforms') {
 				$cf7Container.hide();
+				$kaliformsContainer.hide();
 			}
 
 			if ( $pluginSelect.length && control.settings.plugin_select.get() === 'cf7') {
+				$wpformsContainer.hide();
+				$kaliformsContainer.hide();
+			}
+			
+			if ( $pluginSelect.length && control.settings.plugin_select.get() === 'kali-forms'){
+				$cf7Container.hide();
 				$wpformsContainer.hide();
 			}
 
 			$pluginSelect.change(function() {
 				var val = $( this ).val();
 
-				if ( val == 'wpforms' ) {
-					$wpformsContainer.show().find('option:eq(0)').prop('selected', true);
-					$cf7Container.hide();
-				}
-				else {
-					$wpformsContainer.hide();
-					$cf7Container.show().find('option:eq(0)').prop('selected', true);
+				switch($val) {
+					case 'wpforms':
+						$wpformsContainer.show().find('option:eq(0)').prop('selected', true);
+						$cf7Container.hide();
+						$kaliformsContainer.hide();
+						break;
+					case 'cf7':
+						$wpformsContainer.hide();
+						$kaliformsContainer.hide();
+						$cf7Container.show().find('option:eq(0)').prop('selected', true);
+						break;
+					case 'kali-forms':
+						$wpformsContainer.hide();
+						$cf7Container.hide();
+						$kaliformsContainer.show().find('option:eq(0)').prop('selected', true);
+						break;
 				}
 			} );
-
+			$kaliformsContainer.find('select').change(function() {
+				var val = $( this ).val();
+				if ( isNaN( val ) ) {
+					return;
+				}
+				control.settings.plugin_select( 'kali-forms' );
+				control.settings.form_id( val );
+			});
+			
 			$wpformsContainer.find('select').change(function() {
 				var val = $( this ).val();
 				if ( isNaN( val ) ) {
