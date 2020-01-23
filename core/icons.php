6c72e55cd3c3339f5e0c0f50ca007069b07469a5
AndylukAK <alexandru.tirim@yahoo.com>
2020-01-23 10:03:30 +0200

    https://github.com/MachoThemes/antreas/issues/20

--- core/icons.php
+++ core/icons.php
@@ -13,12 +13,16 @@ function antreas_icon( $value, $wrapper = '', $echo = true ) {
 
 		if( isset( $icon_pack['fontawesomefree']['icons'][html_entity_decode($value)] ) ){
 
-			$font_library = 'fontawesomebrands';
+			$font_library = 'fontawesomefree';
 			$font_value   = $value;
 
+		}else if( $icon_pack['fontawesomebrands']['icons'][html_entity_decode($value)] ){
+
+			$font_library = 'fontawesomebrands';
+			$font_value   = $value;
 		}else {
 
-			$font_library = 'fontawesomefree';
+			$font_library = 'fontawesome';
 			$font_value   = $value;
 		}
 
@@ -26,16 +30,19 @@ function antreas_icon( $value, $wrapper = '', $echo = true ) {
 
 		$icon_data    = explode( '-', $value);
 		$icon_data[1] = html_entity_decode( $icon_data[1]);
-		
 		if( $icon_data[0] == 'fontawesome' ){
 
 			if( isset( $icon_pack['fontawesomebrands']['icons'][$icon_data[1]] ) ){
 				$font_library = 'fontawesomebrands';
 				$font_value   = $icon_data[1];
 
-			}else{
+			}else if ( isset( $icon_pack['fontawesomefree']['icons'][$icon_data[1]] ) ){
 				
 				$font_library = 'fontawesomefree';
+				$font_value   = $icon_data[1];
+			}else {
+
+				$font_library = 'fontawesome';
 				$font_value   = $icon_data[1];
 			}
 		}
@@ -64,7 +71,7 @@ function antreas_icon( $value, $wrapper = '', $echo = true ) {
 //Retrieve the correct library
 function antreas_get_icon( $library, $value ) {
 $result = '';
-
+var_dump( $library);
 switch ( $library ) {
 	case 'fontawesomefree':
 		$result = antreas_icon_library_fontawesome( $value );
@@ -72,16 +79,24 @@ switch ( $library ) {
 	case 'fontawesomebrands':
 		$result = antreas_icon_library_fontawesome_brands( $value );
 		break;
+	case 'fontawesome':
+		$result = antreas_icon_library_fontawesome_exceptions( $value );
 	default:
 		$result = antreas_icon_library_fontawesome( $value );
 		break;
 }
+
 return $result;
 }
 //Icon library for fontawesome
 function antreas_icon_library_fontawesome( $value ) {
 	wp_enqueue_style( 'antreas-fontawesome' );
 	return '<span style="font-family:\'Font Awesome 5 Free\'; font-weight: 900;">' . $value . '</span>';
+}
+
+function antreas_icon_library_fontawesome_exceptions( $value ) {
+	wp_enqueue_style( 'antreas-fontawesome-old' );
+	return '<span style="font-family:\'fontawesome\'; ">' . $value . '</span>';
 }
 
 function antreas_icon_library_fontawesome_brands( $value ) {
